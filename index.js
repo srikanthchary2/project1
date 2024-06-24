fd8d73e014d827cb0dc268d50e6fc594102e78ea
srikanthchary2 <manchanpally.srikanth1@gmail.com>
2024-06-24 14:19:40 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+
+// Require the HTTP module to create an HTTP server.
+const http = require('http');
+
+// Configure the HTTP server to respond with "Hello, World!" to all requests.
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1 (localhost)
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+});
